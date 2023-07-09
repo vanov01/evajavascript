1602a7bfedac598748059e167f84fd607ac05345
EVANS <kiprotichluka70@gmail.com>
2023-07-09 15:26:52 +0300

    initialize the repo and add index.html and jav.js

---
+++ jav.js
@@ -0,0 +1,6 @@
+let greeting ="hello"
+console.log(greeting)
+document.write("Understanding finally")
+alert("favoriteBet")
+const favoriteBet = prompt("What is your favoriteBet?")
+console.log(favoriteBet)
